48f5eed967d858b9f631b42a2052d4bffd9df9c6
Khoa <khoaho120703@gmail.com>
2025-07-19 15:27:38 +0700

    push

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
 
   return (
     <StepContext.Provider value={{ currentStep, setCurrentStep }}>
-      <div className="flex flex-col justify-between items-center h-screen max-h-screen overflow-hidden bg-primary px-4 sm:px-6 md:px-10 relative">
+      <div className="flex flex-col justify-between items-center h-[100dvh] max-h-screen overflow-hidden bg-primary px-4 sm:px-6 md:px-10 relative pb-10">
         <div className="w-full max-w-screen-xl flex-grow overflow-hidden">
           <Navbar />
           {/* Outlet should scroll internally if needed */}
@@ -20,7 +20,8 @@ const Layout = () => {
 
         {/* Button fixed to bottom center */}
         {currentStep !== 6 && (
-          <div className="fixed bottom-20 left-1/2 -translate-x-1/2 z-50">
+          // <div className="fixed bottom-14 left-1/2 -translate-x-1/2 z-50">
+          <div className="">
             <Button />
           </div>
         )}
